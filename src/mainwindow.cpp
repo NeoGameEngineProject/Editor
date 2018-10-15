3f56a2a3281f2b9c87ef4938ffa4b35af8ef7f4c
Yannick Pflanzer <yp1995@outlook.com>
2018-10-15 09:52:37 +0200

    + Implemented "Save" and "Save as"

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -21,12 +21,46 @@ MainWindow::MainWindow(QWidget *parent) :
 
 	resetView();
 
+	m_consoleStream.setOutput(ui->consoleOutput);
+	Neo::Log::setOutStream(m_consoleStream);
+	Neo::Log::setErrStream(m_consoleStream);
+
 	auto level = std::make_shared<Neo::Level>();
 	ui->sceneEditor->setLevel(level);
 	ui->levelTree->setLevel(level);
 	
 	connect(this, &MainWindow::openLevel, [this](QString file) {
-		ui->sceneEditor->getLevel()->load(file.toUtf8().data());
+
+		LOG_INFO("Loading from file: " << file.toStdString());
+		
+		auto level = std::make_shared<Neo::Level>();
+		ui->sceneEditor->setLevel(level);
+		ui->levelTree->setLevel(level);
+
+		if(file.endsWith(".nlv")) // Load the native binary format if possible
+			ui->sceneEditor->getLevel()->loadBinary(file.toUtf8().data());
+		else // Otherwise load compatible format as read-only to prevent accidental conversion
+		{
+			ui->sceneEditor->getLevel()->load(file.toUtf8().data());
+			m_readOnly = true;
+		}
+		
+		this->setWindowTitle(tr("Neo Editor") + " - " + file);
+		m_file = file.toStdString();
+	
+		emit levelChanged();
+	});
+	
+	connect(this, &MainWindow::saveLevel, [this](QString file) {
+		
+		LOG_INFO("Saving to file: " << file.toStdString());
+		
+		ui->sceneEditor->getLevel()->saveBinary(file.toUtf8().data());
+		this->setWindowTitle(tr("Neo Editor") + " - " + file);
+
+		m_file = file.toStdString();
+		m_readOnly = false;
+		
 		emit levelChanged();
 	});
 	
@@ -56,6 +90,8 @@ MainWindow::MainWindow(QWidget *parent) :
 		ui->levelTree->levelChangedSlot();
 		ui->objectWidget->updateObject(h);
 	});
+	
+	emit openLevel("/home/yannick/NeoDev/components/Editor/SDK/testgame/assets/test.dae");
 }
 
 MainWindow::~MainWindow()
@@ -66,15 +102,18 @@ MainWindow::~MainWindow()
 void MainWindow::resetView()
 {
 	splitDockWidget(ui->sceneDock, ui->editorDock, Qt::Orientation::Horizontal);
+	splitDockWidget(ui->editorDock, ui->consoleDock, Qt::Orientation::Vertical);
 	splitDockWidget(ui->sceneDock, ui->objectDock, Qt::Orientation::Vertical);
 
 	tabifyDockWidget(ui->editorDock, ui->gameDock);
+
 	ui->editorDock->raise();
 	
 	// Because resize does not work...
 	ui->sceneDock->setMaximumWidth(0.15*width());
 	ui->objectDock->setMaximumWidth(0.15*width());
-	
+	ui->consoleDock->setMaximumHeight(0.15*height());
+
 	ui->sceneDock->setMaximumHeight(height() * 0.5);
 	ui->objectDock->setMaximumHeight(height() * 0.5);
 }
@@ -83,20 +122,45 @@ void MainWindow::resizeEvent(QResizeEvent* e)
 {
 	ui->sceneDock->setMaximumHeight(height());
 	ui->objectDock->setMaximumHeight(height());
-	
+
+	ui->consoleDock->setMaximumHeight(ui->consoleDock->height());
 	ui->sceneDock->setMaximumWidth(ui->sceneDock->width());
 	ui->objectDock->setMaximumWidth(ui->objectDock->width());
-	
+
 	QMainWindow::resizeEvent(e);
 	
 	ui->sceneDock->setMaximumWidth(width());
 	ui->objectDock->setMaximumWidth(width());
+	ui->consoleDock->setMaximumHeight(height());
 }
 
 void MainWindow::openLevelSlot()
 {
-	auto file = QFileDialog::getOpenFileName(this, tr("Open Level"), "/home", tr("Neo Level (*.nlv);;COLLADA DAE (*.dae)"));
+	auto file = QFileDialog::getOpenFileName(this, tr("Open Level"), ".", tr("Neo Level (*.nlv);;COLLADA DAE (*.dae)"));
+	if(file.isEmpty())
+		return;
+	
 	emit openLevel(file);
+}
+
+void MainWindow::saveLevelAsSlot()
+{
+	auto file = QFileDialog::getSaveFileName(this, tr("Save Level"), ".", tr("Neo Level (*.nlv)"));
+	if(file.isEmpty())
+		return;
+	
+	emit saveLevel(file);
+}
+
+void MainWindow::saveLevelSlot()
+{
+	if(m_readOnly)
+	{
+		QMessageBox::critical(this, tr("Save"), tr("Can not save: The file is loaded as read-only! Try 'Save As' to select another target."));
+		return;
+	}
+
+	emit saveLevel(QString(m_file.c_str()));
 }
 
 void MainWindow::translationTool()
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -6,6 +6,8 @@
 
 #include <memory>
 
+#include <ConsoleStream.h>
+
 namespace Ui 
 {
 class MainWindow;
@@ -33,13 +35,19 @@ signals:
 	
 public slots:
 	void openLevelSlot();
-	
+	void saveLevelSlot();
+	void saveLevelAsSlot();
+
 	void translationTool();
 	void rotationTool();
 	void scaleTool();
 	
 private:
 	Ui::MainWindow *ui;
+	ConsoleStream m_consoleStream;
+	
+	std::string m_file; // The file that is currently being edited
+	bool m_readOnly = false; // If the file is loaded as read-only (e.g. for DAE files)
 };
 
 #endif // MAINWINDOW_H

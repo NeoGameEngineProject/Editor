9cf6e680d2b4f4e6a01980d43409f1b97599a2e5
Yannick Pflanzer <ypflanzer@outlook.com>
2020-01-22 11:09:50 +0100

    * Also build packages and upload to deployment server

--- scripts/build_appimage.sh
+++ scripts/build_appimage.sh
@@ -10,6 +10,7 @@ rm -rf AppDir
 cmake .. -DCMAKE_BUILD_TYPE=Release -DNO_TESTS=TRUE -DCMAKE_INSTALL_PREFIX=/usr
 make -j$(nproc)
 make install DESTDIR=$PWD/AppDir
+make package
 
 export LD_LIBRARY_PATH=$PWD/AppDir/usr/lib/
 linuxdeploy.AppImage --appdir AppDir --output appimage -d ../scripts/NeoEditor.desktop -i ../scripts/neo-icon.png --plugin qt

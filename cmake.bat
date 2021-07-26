b1e7c3245450e6384bac4174a63ac82eabee1289
Yannick Pflanzer <yp1995@outlook.com>
2021-07-26 10:45:37 +0200

    Some changes for Windows support

--- cmake.bat
+++ cmake.bat
@@ -3,5 +3,5 @@ rem scripts\install_vcpkg.bat
 
 mkdir build
 cd build
-cmake .. -G "Visual Studio 15 2017 Win64" -DNO_NET=TRUE -DNO_VR=TRUE -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PROGRAM_PATH=C:/dev/vcpkg/installed/x64-windows/tools/lua -DCMAKE_TOOLCHAIN_FILE=C:/dev/vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows
+cmake .. -DNO_NET=TRUE -DNO_VR=TRUE -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PROGRAM_PATH=C:/dev/vcpkg/installed/x64-windows/tools/lua -DCMAKE_TOOLCHAIN_FILE=C:/dev/vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows
 pause
--- scripts/install-vcpkg.bat
+++ scripts/install-vcpkg.bat
@@ -3,4 +3,4 @@ git clone https://github.com/microsoft/vcpkg.git
 cd vcpkg
 
 bootstrap-vcpkg.bat -disableMetrics
-vcpkg.exe install qt5
+vcpkg.exe install qt5:x64-windows qt5-winextras:x64-windows
